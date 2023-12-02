1918a7470070836468a5725d614c2b990f696ab2
TaopikQM <92729541+TaopikQM@users.noreply.github.com>
2023-12-02 22:33:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,32 +5,35 @@ def generate_key(seed, length):
     key = hashlib.sha256(seed.encode()).digest()[:length]
     return key
 
-def encrypt_decrypt(message, key, is_encrypt=True):
-    result = bytearray()
+def encrypt_decrypt(message, key):
+    encrypted = bytearray()
     for i in range(len(message)):
-        if is_encrypt:
-            result.append(message[i] ^ key[i % len(key)])
-        else:
-            result.append(message[i] ^ key[i % len(key)])
-    return bytes(result)
+        encrypted.append(message[i] ^ key[i % len(key)])
+    return bytes(encrypted)
 
 def main():
-    message_input = st.text_input("Masukkan pesan:")
-    seed_input = st.text_input("Masukkan seed untuk kunci:")
-    status = st.radio("Pilih Aksi:", ["Enkripsi", "Dekripsi"])
+    # Input pesan yang akan dienkripsi
+    message = st.text_input("Masukkan pesan:")
 
-   
-    if st.button("Proses"):
-        key_length = len(message_input)
-        key = generate_key(seed_input, key_length)
+    # Input kunci (gunakan seed untuk menghasilkan kunci yang sama setiap kali program dijalankan)
+    seed = st.text_input("Masukkan seed untuk kunci:")
+
+    if encrypt_decrypt_button:
+        key_length = len(message)
+        key = generate_key(seed, key_length)
 
         # Proses enkripsi
-        encrypted_message = encrypt_decrypt(message.encode(), key)
-        st.write("Pesan terenkripsi:", encrypted_message)
+        encrypted_message = encrypt_decrypt(message.encode(), key, is_encrypt=True)
+        st.write("Pesan terenkripsi:", encrypted_message.hex())
 
         # Proses dekripsi
-        decrypted_message = encrypt_decrypt(encrypted_message, key)
-        st.write("Pesan terdekripsi:", decrypted_message.decode())
+        try:
+            decrypted_message = encrypt_decrypt(encrypted_message, key, is_encrypt=False)
+            st.write("Pesan terdekripsi:", decrypted_message.decode())
+        except UnboundLocalError:
+            st.error("Anda perlu mengenkripsi pesan terlebih dahulu sebelum dapat mendekripsinya.")
+
 
 if __name__ == "__main__":
     main()
+    
