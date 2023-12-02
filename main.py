04f307bb09492d993b401decf5c581bb88ca8c92
TaopikQM <92729541+TaopikQM@users.noreply.github.com>
2023-12-02 20:44:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,23 +12,25 @@ def encrypt_decrypt(message, key):
     return bytes(encrypted)
 
 def main():
+    st.title("Aplikasi Enkripsi & Dekripsi")
+
     # Input pesan yang akan dienkripsi
     message = st.text_input("Masukkan pesan:")
-    
+
     # Input kunci (gunakan seed untuk menghasilkan kunci yang sama setiap kali program dijalankan)
     seed = st.text_input("Masukkan seed untuk kunci:")
-    
-    if st.button("Enkripsi"):
+
+    if st.button("Enkripsi / Dekripsi"):
         key_length = len(message)
         key = generate_key(seed, key_length)
 
-        # Proses enkripsi
-        encrypted_message = encrypt_decrypt(message.encode(), key)
-        st.write("Pesan terenkripsi:", encrypted_message)
-
-    if st.button("Dekripsi"):
-        decrypted_message = encrypt_decrypt(encrypted_message, key)
-        st.write("Pesan terdekripsi:", decrypted_message.decode())
+        # Proses enkripsi atau dekripsi
+        result = encrypt_decrypt(message.encode(), key)
+        
+        if isinstance(result, str):
+            st.write("Pesan terdekripsi:", result)
+        else:
+            st.write("Pesan terenkripsi:", result.decode())
 
 if __name__ == "__main__":
     main()
