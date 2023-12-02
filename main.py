82360ca2028b33813aa958d8fe8cb3fdef9e9628
TaopikQM <92729541+TaopikQM@users.noreply.github.com>
2023-12-02 19:38:58 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import hashlib
+
+def generate_key(seed, length):
+    key = hashlib.sha256(seed.encode()).digest()[:length]
+    return key
+
+def encrypt_decrypt(message, key):
+    encrypted = bytearray()
+    for i in range(len(message)):
+        encrypted.append(message[i] ^ key[i % len(key)])
+    return bytes(encrypted)
+
+def main():
+    st.title("Stream Cipher with Streamlit")
+
+    # Input pesan yang akan dienkripsi
+    message = st.text_input("Masukkan pesan:")
+
+    # Input kunci (gunakan seed untuk menghasilkan kunci yang sama setiap kali program dijalankan)
+    seed = st.text_input("Masukkan seed untuk kunci:")
+    
+    if message and seed:
+        key_length = len(message)
+        key = generate_key(seed, key_length)
+
+        # Proses enkripsi
+        encrypted_message = encrypt_decrypt(message.encode(), key)
+        st.write("Pesan terenkripsi:", encrypted_message.hex())
+
+        # Proses dekripsi
+        decrypted_message = encrypt_decrypt(encrypted_message, key)
+        st.write("Pesan terdekripsi:", decrypted_message.decode())
+
+if __name__ == "__main__":
+    main()
